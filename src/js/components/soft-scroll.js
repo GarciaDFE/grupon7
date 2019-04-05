d47d8a447c08eb2d5ad836485dc52a6ad581c417
GarciaDFE <garcia.dfe@gmail.com>
2019-04-05 18:01:28 -0300

    Ajustes last-child nos Cards e merge com commit da animação dos botões to top do Raphael

--- src/js/components/soft-scroll.js
+++ src/js/components/soft-scroll.js
@@ -1,25 +1,50 @@
-$(".soft-scroll1").on("click", function(e) {
-  e.preventDefault();
-  let id = $(this).attr("href"),
-    targetOffset = $(id).offset().top;
+const $originServices = document.querySelector(".-originservices");
+const $originPartners = document.querySelector(".-originpartners");
+const $wrapServices = document.querySelector(".wrap-services");
+const $wrapPartners = document.querySelector(".wrap-partners");
+const $btnV1 = document.querySelector(".-v1");
+const $btnV2 = document.querySelector(".-v2");
 
-  $("html, body").animate(
-    {
-      scrollTop: targetOffset - 100
-    },
-    1500
-  );
+console.log($wrapPartners);
+console.log($originPartners);
+
+$originServices.addEventListener("click", function () {
+  smoothScroll($wrapServices, 1000);
 });
 
-$(".soft-scroll2").on("click", function(e) {
-  e.preventDefault();
-  let id = $(this).attr("href"),
-    targetOffset = $(id).offset().top;
-
-  $("html, body").animate(
-    {
-      scrollTop: targetOffset - 100
-    },
-    2000
-  );
+$btnV1.addEventListener("click", function () {
+  smoothScroll($originServices, 5000);
 });
+
+$originPartners.addEventListener("click", function () {
+  smoothScroll($wrapPartners, 2000);
+});
+
+$btnV2.addEventListener("click", function () {
+  smoothScroll($originServices, 6000);
+});
+
+const smoothScroll = (target, duration) => {
+  const $target = target;
+  const $targetPosition = $target.getBoundingClientRect().top;
+  const $startPosition = window.pageYOffset;
+  const distance = $targetPosition - $startPosition;
+  let startTime = null;
+
+  const animation = currentTime => {
+    if (startTime === null) startTime = currentTime;
+    const timeElapsed = currentTime - startTime;
+    const run = ease(timeElapsed, $startPosition, distance, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  };
+
+  const ease = (t, b, c, d) => {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  };
+
+  requestAnimationFrame(animation);
+};
