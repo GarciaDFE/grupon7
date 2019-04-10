5b82a808e9bbdd834810512b24cf628c81ce73ba
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-04-10 20:09:26 +0000

    1 - Na pasta src/img inclusão dos arquivos arrow-left.svg e arrow-right.svg
    2 - Criação dos arquivos
    src/js/tools/glide.min.js
    src/scss/objects/wrap-glide.scss  (redefine certos componentes dos
    arquivos glide.core.min.scss e glide.theme.min.scss)
    src/scss/objects/tools/gide.core.min.scss
    src/scss/objects/tools/gide.theme.min.scss
    
    3 - Troca do arquivo main-carousel.js
    
    4 - Troca do arquivo src/scss/components/main-carousel.scss
    
    5 - No index.html
    Inserção de link jss para glide.min.js
    
    6 - No arquivo syless.scss
    // importando pasta tools
    @import 'tools/glide.core.min.scss';
    @import 'tools/glide.theme.min.scss';
    
    // importando pasta objects
    @import 'objects/wrap-header.scss';
    @import 'objects/brand-nav.scss';
    @import 'objects/wrap-glide.scss'; ----> Deve ser incluido nesta posição
    
    7 - Alteração significativa do arquivo index.html das linhas 52 a 83,
    pois exige a inclusao de elementos do glide tanto em codigo quanto em
    classe
    
    8 - inclusão de img {max-width: 100%} em src/base/_styles.scss
    
    9 - Na pasta src/img inclusão dos arquivos arrow-left.svg e
    arrow-right.svg

--- src/js/components/main-carousel.js
+++ src/js/components/main-carousel.js
@@ -1,20 +1,11 @@
-var $bullet = document.querySelectorAll(".bullet");
-var $bulletIsActive = document.querySelector(".bullet.-isActive");
-var $cardIsActive = document.querySelector(".card.-isActive");
+let $slide = Math.floor(Math.random() * 3);
 
-$bullet.forEach((item) => {
-   item.addEventListener("click", clickInBullet)
-})
-
-function clickInBullet() {
-   var $bulletClicked = this
-   $bulletIsActive.classList.remove("-isActive"); /* desativa o bullet anterior */
-   $bulletClicked.classList.add("-isActive"); /* ativa o bullet clicado */
-   $bulletIsActive = $bulletClicked; /* seta o novo bullet ativo */
-
-   $cardIsActive.classList.remove("-isActive"); /* desativa o card anterior */
-   var idCard = $bulletClicked.querySelector("a").getAttribute("href"); /* guarda na variável o atributo "href" associado ao card que ficará ativo de acordo com o novo bullet ativado com o último click */
-   var $targetCard = document.querySelector(idCard); /* Seleciona qual o card que ficará ativo de acordo com a variável "idCard" */
-   $targetCard.classList.add("-isActive"); /* ativa o card do bullet clicado relacionado */
-   $cardIsActive = $targetCard; /* seta o novo card ativo */     
-}
+new Glide(".glide", {
+  type: "carousel",
+  gap: 0,
+  startAt: $slide,
+  perView: 1,
+  autoplay: 5000,
+  hoverpause: true,
+  animationDuration: 1000
+}).mount();
