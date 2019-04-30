ff3db7c9eea950143ad0289177709759b3e79400
GarciaDFE <garcia.dfe@gmail.com>
2019-04-30 15:06:25 -0300

    Implementado novo botão flutuante para o topo, início de ajustes nas funcionalidades do menu e incluido material complementar entregue por Cristiano 30-04

--- src/js/components/soft-scroll.js
+++ src/js/components/soft-scroll.js
@@ -1,42 +1,71 @@
-// const $originServices = document.querySelector(".-originservices");
-// const $originPartners = document.querySelector(".-originpartners");
-// const $originFooter = document.querySelector(".-originfooter");
-// const $Services = document.querySelector(".wrap-services");
-// const $Partners = document.querySelector(".wrap-partners");
-// const $Contacts = document.querySelector(".main-footer");
-// const $btnV1 = document.querySelector(".-v1");
-// const $btnV2 = document.querySelector(".-v2");
-// let $view = 0;
-// let breakPhone = 599;
+// BOTÃO FLUTUTANTE TO TOP & EFEITO REDUÇÃO HEADER
+const $wrapHeader = document.querySelector(".wrap-header");
+const $btnToTop = document.querySelector(".btn-totop");
+const landmark = 300;
+// NEVEGAÇÃO NA PÁGINA COM EFEITO DE SCROLL SUAVE
+const $itemServicos = document.querySelector("#itemServicos");
+const $itemParceiros = document.querySelector("#itemParceiros");
+const $itemContato = document.querySelector("#itemContato");
+const $Services = document.querySelector(".wrap-services");
+const $Partners = document.querySelector(".wrap-partners");
+const $Contacts = document.querySelector(".main-footer");
+let $positionServices = 0;
+let $positionPartners = 0;
+let $positionContacts = 0;
 
-// $originServices.addEventListener("click", function() {
-//   $view = window.innerWidth;
-//   if ($view > breakPhone) {
-//     smoothScroll($Services, 1000);
-//   }
-// });
+const breakPhone = 599;
+const descontoAlturaHeader = 80;
 
-// $originPartners.addEventListener("click", function() {
-//   smoothScroll($Partners, 2000);
-// });
+// BOTÃO FLUTUTANTE TO TOP & EFEITO REDUÇÃO HEADER
+window.onscroll = function() {
+  var top = window.pageYOffset;
+  if (top > landmark) {
+    $btnToTop.classList.add("-visible");
+    $wrapHeader.classList.add("scrolleffect");
+  } else {
+    $btnToTop.classList.remove("-visible");
+    $wrapHeader.classList.remove("scrolleffect");
+  }
+};
 
-// $originFooter.addEventListener("click", function() {
-//   smoothScroll($Contacts, 2000);
-// });
+$btnToTop.addEventListener("click", function() {
+  smoothScroll($wrapHeader, 2000, 0);
+});
 
-// $btnV1.addEventListener("click", function() {
-//   smoothScroll($originServices, 2000);
-// });
+//NEVEGAÇÃO NA PÁGINA COM EFEITO DE SCROLL SUAVE
+if ($Services != null) {
+  $positionServices = $Services.getBoundingClientRect().top;
+}
 
-// $btnV2.addEventListener("click", function() {
-//   smoothScroll($originServices, 2000);
-// });
+if ($Partners != null) {
+  $positionPartners = $Partners.getBoundingClientRect().top;
+}
 
-const smoothScroll = (target, duration) => {
-  const $target = target;
-  const $targetPosition = $target.getBoundingClientRect().top;
-  const $startPosition = window.pageYOffset;
-  const distance = $targetPosition - $startPosition;
+if ($Contacts != null) {
+  $positionContacts = $Contacts.getBoundingClientRect().top;
+}
+
+$itemServicos.addEventListener("click", function() {
+  $view = window.innerWidth;
+  if ($view > breakPhone && $Services != null) {
+    smoothScroll($Services, 2000, $positionServices - descontoAlturaHeader);
+  }
+});
+
+$itemParceiros.addEventListener("click", function() {
+  smoothScroll($Partners, 2000, $positionPartners - descontoAlturaHeader);
+});
+
+$itemContato.addEventListener("click", function() {
+  smoothScroll($Contacts, 2000, $positionContacts - descontoAlturaHeader);
+});
+
+// CONTROLA O EFEITO DE SCROLL SUAVE
+const smoothScroll = (target, duration, position) => {
+  let $target = target;
+  let $targetPosition = position;
+  let $startPosition = window.pageYOffset;
+  let distance = $targetPosition - $startPosition;
   let startTime = null;
 
   const animation = currentTime => {
